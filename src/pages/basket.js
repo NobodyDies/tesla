d2bec55003d5beeac30440a0d10a9115e5aecd95
ArtoriasVictrix <ArtoriasVictrix>
2017-12-21 04:44:16 +0500

    Полная адаптивность страницы - Корзина

--- src/pages/basket.js
+++ src/pages/basket.js
@@ -1,5 +1,12 @@
-$( document ).ready(() => {
+$( document ).ready((e) => {
 
+	$('#next-step').click(function (e) {
+		$('.basket_page-content-right').slideUp();
+		$('.basket_page-content-left').slideDown();
 
+		$('.basket_page-content-right').addClass( "hidden-basket" );
+
+		window.scrollTo(0,0);
+	});
 
 });
