2c105bff3946237bd992a766c19c1c44ea3757ff
Khamit Rakhimkulov <nobodydies.ufa@gmail.com>
2018-02-07 03:53:30 +0300

    slider in progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import { contain, cover } from "contain-cover";
+
 $( document ).ready(() => {
 
 	// fixed/absolute position for navigation
@@ -29,29 +31,65 @@ $(window).on('load', function(){
 		columnWidth: '.grid-sizer'
 	});
 
-	// var mainSlider = new Swiper ('.index_page-slider', {
-	// 	// Optional parameters
-	// 	autoplay: true,
-	// 	slidesPerView: 1,
-	// 	slidesPerGroup: 1,
-	// 	speed: 1000,
-	// 	delay: 1000,
-	// 	spaceBetween: 0,
-	// 	loop: true,
-	// 	effect: 'fade',
-	// 	fadeEffect: {
-	// 		crossFade: true
-	// 	},
-	// 	// Navigation arrows
-	// });
+	window.mainSlider = new Swiper ('.index_page-slider', {
+		// Optional parameters
+		//autoplay: true,
+		slidesPerView: 1,
+		slidesPerGroup: 1,
+		speed: 1000,
+		delay: 1000,
+		spaceBetween: 0,
+		virtualTranslate: true,
+		loop: true,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		},
+		// Navigation arrows
+	});
+	mainSlider.on('slideChange', () => {
+		console.log('slideChange');
+		console.log(mainSlider.activeIndex);
+		console.log(mainSlider.previousIndex);
+		var slide = $(mainSlider.slides[mainSlider.previousIndex]);
+		var image = slide.find('.index_page-slider-item--image')[0];
+
+		let { width, height, x, y } = cover(window.innerWidth, window.innerHeight, image.naturalWidth, image.naturalHeight);
+
+		var canvas = document.createElement('canvas');
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight/2;
+		var ctx = canvas.getContext('2d');
+		ctx.drawImage(image, x, y, width, height);
+		slide.append(canvas);
+
+		var canvas2 = document.createElement('canvas');
+		canvas2.width = window.innerWidth;
+		canvas2.height = window.innerHeight/2;
+		var ctx2 = canvas2.getContext('2d');
+		ctx2.drawImage(image, x, -window.innerHeight/2+y, width, height);
+		slide.append(canvas2)
+
+		$(image).css({display: 'none'})
+	})
+
+	mainSlider.on('slideChangeTransitionEnd', () => {
+		console.log('slideChangeTransitionEnd');
+		console.log(mainSlider.activeIndex);
+		console.log(mainSlider.previousIndex);
+		var slide = $(mainSlider.slides[mainSlider.previousIndex]);
+		var image = slide.find('.index_page-slider-item--image')[0];
+		$(image).css({display: 'block'})
+		slide.find('canvas').remove();
+	})
 
 	var mySwiper = new Swiper ('.swiper-container', {
 	    // Optional parameters
 	    autoplay: true,
 	    slidesPerView: 3,
 	    slidesPerGroup: 3,
-	    speed: 900,
-	    delay: 7000,
+	    speed: 9000,
+	    delay: 20000,
 	    spaceBetween: 40,
 	    loop: true,
 		breakpoints: {
