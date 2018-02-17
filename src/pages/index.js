7aa4cd64a9955a4a3f932b6768ffcef2ed89717b
ArtoriasVictrix <simpforreg@yandex.ru>
2018-02-18 04:38:08 +0500

    Тупанул, увы
    Случайно натворил ерунды в гите, но сейчас всё поправил, работает + добавил задержку в 5 секунд между слайдами

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,9 @@ $(window).on('load', function(){
 
 	window.mainSlider = new Swiper ('.index_page-slider', {
 		// Optional parameters
-		autoplay: true,
+		autoplay: {
+			delay: 5000,
+		},
 		slidesPerView: 1,
 		slidesPerGroup: 1,
 		speed: 1,
@@ -46,20 +48,20 @@ $(window).on('load', function(){
 		effect: 'fade',
 		fadeEffect: {
 			crossFade: true
-		}
+		},
 		// Navigation arrows
 	});
 	mainSlider.on('slideChangeTransitionStart', () => {
-		var prev = $(mainSlider.slides[mainSlider.previousIndex]);
-		prev.css({opacity: 1, 'z-index': 0}).addClass('removing').removeClass('inserting');
-		var slide = $(mainSlider.slides[mainSlider.activeIndex]);
-		slide.addClass('inserting').css({opacity: 1, 'z-index': 1});
+		console.log('slideChangeTransitionStart');
+		console.log(mainSlider.activeIndex);
+		console.log(mainSlider.previousIndex);
+		var slide = $(mainSlider.slides[mainSlider.previousIndex]);
 		var image = slide.find('.index_page-slider-item--image')[0];
 
-		let { width, height, x, y } = cover(document.body.clientWidth, window.innerHeight, image.naturalWidth, image.naturalHeight);
+		let { width, height, x, y } = cover(window.innerWidth, window.innerHeight, image.naturalWidth, image.naturalHeight);
 
 		var canvas = document.createElement('canvas');
-		canvas.width = document.body.clientWidth;
+		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight/2;
 		var ctx = canvas.getContext('2d');
 		ctx.drawImage(image, x, y, width, height);
@@ -68,7 +70,7 @@ $(".myinner").append(canvas);
 //mainSlider.slideNext()
 
 		var canvas2 = document.createElement('canvas');
-		canvas2.width = document.body.clientWidth;
+		canvas2.width = window.innerWidth;
 		canvas2.height = window.innerHeight/2;
 		var ctx2 = canvas2.getContext('2d');
 		ctx2.drawImage(image, x, -window.innerHeight/2+y, width, height);
@@ -79,10 +81,10 @@ canvas2 = $(".myinner").append(canvas2);
 	})
 
 	mainSlider.on('slideChangeTransitionEnd', () => {
-		var prev = $(mainSlider.slides[mainSlider.previousIndex]);
-		prev.css({opacity: 0, 'z-index': 0}).removeClass('removing').removeClass('inserting');
-		var slide = $(mainSlider.slides[mainSlider.activeIndex]);
-		slide.css({opacity: 1, 'z-index': 1}).removeClass('removing').removeClass('inserting');
+		console.log('slideChangeTransitionEnd');
+		console.log(mainSlider.activeIndex);
+		console.log(mainSlider.previousIndex);
+		var slide = $(mainSlider.slides[mainSlider.previousIndex]);
 		var image = slide.find('.index_page-slider-item--image')[0];
 //$(image).css({display: 'block'})
 
