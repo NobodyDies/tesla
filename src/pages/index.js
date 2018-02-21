e8e1b2a8310d1f26e1ed4dd908ee5935593547e7
ArtoriasVictrix <ArtoriasVictrix>
2018-02-21 22:49:56 +0500

    Определение осла и его братца

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,22 @@ $(document).ready(() => {
 		}
 	});
 
+
+	if (/MSIE 10/i.test(navigator.userAgent)) {
+	   // This is internet explorer 10
+	   window.alert('isIE10');
+	}
+
+	if (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+	    window.alert('Oooh! This Browser is awful!');
+	}
+
+	if (/Edge\/\d./i.test(navigator.userAgent)){
+	   // This is Microsoft Edge
+	   window.alert('Microsoft Edge');
+	}
+
+
 });
 
 
